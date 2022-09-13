7fc34d4be38ed44a1228ef91e6b780fe6ee79e3b
Gundancer <112210674+Gundancer@users.noreply.github.com>
2022-09-13 18:51:14 -0500

    fix icons disappearing on app reload for pf2e
    fix icons disappearing on app reload for pf2e

--- classes/TokenColorMarkerPF2e.mjs
+++ classes/TokenColorMarkerPF2e.mjs
@@ -4,20 +4,34 @@ import { TokenColorMarker } from './TokenColorMarker.mjs';
 // specific changes to work for the PF2e system
 export class TokenColorMarkerPF2e extends TokenColorMarker {
 
-    static addTokenColorMarkerModule() {
+    static async addTokenColorMarkerModule() {
         super.addTokenColorMarkerModule();
 
-        // PF2e system sets ActiveEffect "disabled" to true. this resets after the app
-        // is reloaded. loop through actors and set disabled = false on ActiveEffects
-        game.actors.forEach(actor => {
-            actor.effects.forEach(effect => {
-                effect.update({ disabled: false });
-            });
-        });
-    
         // PF2e system sets ActiveEffect "disabled" to true. set disabled = false
         Hooks.on('createActiveEffect', (app, html, data) => { 
             app.update({ disabled: false });
         });
+
+        Hooks.on('drawToken', (token) => { 
+            this.drawActiveEffects(token);
+        });
+    }
+
+    static async drawActiveEffects(token){
+        // PF2e system sets ActiveEffect "disabled" to true. this resets after the app
+        // is reloaded. Set disabled = false on ActiveEffects and draw again if needed.
+        let needsRedraw = false
+        for (const effect of token.actor.effects) {
+            if(effect.disabled) {
+                await effect.updateSource({ disabled: false });
+                needsRedraw = true;
+            }
+        }
+
+        // Only redraw if needed. this also prevents an infinate loop.
+        if(needsRedraw){
+            //Note: I would rather call drawEffects(), but it changes the order and is poor UX.
+            token.draw();
+        }
     }
 }
